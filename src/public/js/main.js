7e3bdb23b548760cdc9e27cb489669d7f99d6e8f
terygasmen <terygasmen@gmail.com>
2023-01-14 14:13:23 +0800

    import alert to main js

--- src/public/js/main.js
+++ src/public/js/main.js
@@ -1,8 +1,13 @@
 import ProductData from "./ProductData.mjs";
 import ProductList from "./ProductList.mjs";
+import Alert from './alert.js';
 
 const dataSource = new ProductData("tents");
 const element = document.querySelector(".product-list");
 const listing = new ProductList("Tents", dataSource, element);
+const alerts = new Alert(); alerts.create();
 
 listing.init();
+
+
+

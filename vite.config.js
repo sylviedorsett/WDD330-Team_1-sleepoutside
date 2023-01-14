96174a5da8648a08e8443612ac81636f4617b5d8
Sylvie Dorsett <sylviejovet@hotmail.com>
2023-01-14 10:15:18 -0700

    Reconfigure of file structure.

--- vite.config.js
+++ vite.config.js
@@ -11,19 +11,10 @@ export default defineConfig({
         main: resolve(__dirname, "src/index.html"),
         cart: resolve(__dirname, "src/cart/index.html"),
         checkout: resolve(__dirname, "src/checkout/index.html"),
-        product1: resolve(
-          __dirname,
-          "src/product_pages/index.html"
-        ),
+        product1: resolve(__dirname, "src/product_pages/index.html"),
         product2: resolve(__dirname, "src/product_pages/index.html"),
-        product3: resolve(
-          __dirname,
-          "src/product_pages/index.html"
-        ),
-        product4: resolve(
-          __dirname,
-          "src/product_pages/index.html"
-        ),
+        product3: resolve(__dirname, "src/product_pages/index.html"),
+        product4: resolve(__dirname, "src/product_pages/index.html"),
       },
     },
   },

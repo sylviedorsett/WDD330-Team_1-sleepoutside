862ce181f7d871d2cbb09ec08d89718986c1635a
Sylvie Dorsett <sylviejovet@hotmail.com>
2023-01-19 14:43:12 -0700

    updated animate cart branch

--- src/js/cart.js
+++ src/js/cart.js
@@ -2,3 +2,18 @@ import ShoppingCart from "./shoppingCart.mjs";
 
 const cart = new ShoppingCart("so-cart", ".product-list");
 cart.renderCartContents();
+
+
+//Animation for Cart Icon when items are added
+document.getElementById("addToCart").addEventListener("click", play);
+
+export function play() {
+    const cart = document.getElementById('cart_div')
+    cart.classList.replace('cart', 'cart-animate');
+    //stop();
+  }
+  
+  export function stop() {
+    const cart = document.getElementById('cart_div')
+    cart.classList.replace('cart-animate', 'cart');
+  }

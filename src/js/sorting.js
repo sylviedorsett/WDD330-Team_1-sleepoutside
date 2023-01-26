edd8084155818abb9483d3d337d75b4fda8fec91
terygasmen <terygasmen@gmail.com>
2023-01-26 14:28:42 +0800

    added functions to sort by price and name

--- src/js/sorting.js
+++ src/js/sorting.js
@@ -1,4 +1,7 @@
-// checks if the sorting is by name or price
+// imports products from productList module
+import { productCardTemplate } from "./productList.mjs";
+
+// CHECKS IF THE SORTING IS BY NAME OR PRICE
 function sortProducts(sortBy) {
     if (sortBy === "name") {
       sortByNameAsc(products);
@@ -6,4 +9,34 @@ function sortProducts(sortBy) {
       sortByPriceAsc(products);
     }
 }
-  
+
+// SORTS BY NAME
+function sortByNameAsc(products) {
+  const products = productCardTemplate();
+
+  // sort the products by name
+  products.sort((a, b) => {
+    if (a.Name < b.Name) {
+      return -1;
+    }
+    if (a.Name > b.Name) {
+      return 1;
+    }
+    return 0;
+  });
+
+  return products;
+}
+
+// SORTS BY PRICE
+function sortByPriceAsc(products) {
+  const products = productCardTemplate();
+
+  // sort the products by price
+  products.sort((a, b) => {
+    return a.FinalPrice - b.FinalPrice;
+  });
+
+  return products;
+}
+
